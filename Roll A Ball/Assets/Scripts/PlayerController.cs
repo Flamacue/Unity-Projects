a643792d0a020ad362ad9b67033ba0e147df1d21
Adam K. Sumner <asumner@hawk.iit.edu>
2016-12-11 13:24:56 -0500

    initial commit

---
+++ Roll A Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody rb;
+    public float speed;
+    private int count;
+    public Text countText;
+    public Text winText;
+
+    public void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        setCountText();
+        winText.text = "";
+    }
+
+    public void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+        rb.AddForce(movement * speed);
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up")){
+            count++;
+            setCountText();
+            other.gameObject.SetActive(false);
+        }
+
+    }
+
+    public void setCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 16){
+            winText.text = "You Win!";
+        }
+
+    }
+}
