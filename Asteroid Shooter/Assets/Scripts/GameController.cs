21ed617962c424dccd3cb530106c67c7ac136309
Adam K. Sumner <asumner@hawk.iit.edu>
2016-12-11 20:32:54 -0500

    add primitive arcade shooter

---
+++ Asteroid Shooter/Assets/Scripts/GameController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject[] hazard;
+    public Vector3 spawnValues;
+    public int hazardcount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
+
+    public GUIText scoreText;
+    private int score;
+
+    public GUIText restartText;
+    public GUIText gameOverText;
+
+    private bool gameOver;
+    private bool restart;
+
+    private void Start()
+    {
+        gameOver = false;
+        restart = false;
+        restartText.text = "";
+        gameOverText.text = "";
+        score = 0;
+        updateScore();
+        StartCoroutine(SpawnWaves());
+    }
+
+    private void Update()
+    {
+        if (restart)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+        }
+    }
+
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (!gameOver)
+        {
+            for (int i = 0; i < hazardcount; i++)
+            {
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), 0.0f, spawnValues.z);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(hazard[Random.Range(0,2)], spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(waveWait);
+        }
+        restartText.text = "Press 'R' for Restart";
+        restart = true;
+    }
+
+    void updateScore()
+    {
+        scoreText.text = "Score: " + score;
+    }
+
+    public void addScore(int score)
+    {
+        this.score += score;
+        updateScore();
+    }
+
+    public void GameOver()
+    {
+        gameOverText.text = "Game Over";
+        gameOver = true;
+    }
+}
---
+++ Asteroid Shooter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class Boundary
+{
+    public float minX, maxX, minZ, maxZ;
+
+}
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody rb;
+    public float speed;
+    public float tilt;
+    public Boundary boundary;
+    public GameObject shot;
+    public Transform shotSpawn;
+    private float nextFire;
+    public float fireRate;
+    private AudioSource audio;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        audio = GetComponent<AudioSource>();
+    }
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.velocity = movement * speed;
+
+
+
+        rb.position = new Vector3(
+            Mathf.Clamp(rb.position.x, boundary.minX, boundary.maxX),
+            0.0f,
+            Mathf.Clamp(rb.position.z, boundary.minZ, boundary.maxZ)
+        );
+
+        rb.rotation = Quaternion.Euler(0.0f, 0.0f, rb.velocity.x * -tilt);
+    }
+
+    private void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+            audio.Play();
+        }
+    }
+}
